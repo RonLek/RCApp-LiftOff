04f26018c66fa03aafca27a1a2a45534157a3afb
Rohan Lekhwani <rohanlekhwani@ymail.com>
2021-03-06 19:07:54 +0530

    Builds Liftoff app

---
+++ LiftoffApp.ts
@@ -0,0 +1,19 @@
+import {
+    IAppAccessors,
+    ILogger,
+    IConfigurationExtend
+} from '@rocket.chat/apps-engine/definition/accessors';
+import { App } from '@rocket.chat/apps-engine/definition/App';
+import { IAppInfo } from '@rocket.chat/apps-engine/definition/metadata';
+
+import { LiftoffCommand } from './commands/liftoff';
+
+export class LiftoffApp extends App {
+    constructor(info: IAppInfo, logger: ILogger, accessors: IAppAccessors) {
+        super(info, logger, accessors);
+    }
+
+    protected async extendConfiguration(configuration: IConfigurationExtend): Promise<void> {
+        await configuration.slashCommands.provideSlashCommand(new LiftoffCommand(this));
+    }
+}
